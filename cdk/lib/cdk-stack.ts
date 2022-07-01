6cfdac76e5b16db786f8ac6f6bab6b640859afb2
Phu Nguyen <ngp130895@gmail.com>
2022-07-01 12:47:41 +0300

    feat: added fargate

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,13 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster }from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,6 +20,29 @@ export class CdkStack extends Stack {
         commands: ['cd cdk', 'npm ci', 'npm run build', 'npx cdk synth'],
         primaryOutputDirectory: 'cdk/cdk.out',
       })
+    });
+
+    const vpc = new Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    // create an image from ../Dockerfile
+    const image = new DockerImageAsset(this, 'DockerImage', {
+      directory: '../',
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: ContainerImage.fromDockerImageAsset(image) },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is false
     });
   }
 }
