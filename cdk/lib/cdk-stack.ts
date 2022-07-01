47a803d67e1a03b305cbe72954bbb82ff927eecc
Phu Nguyen <ngp130895@gmail.com>
2022-07-01 13:40:26 +0300

    revert: remove docker and fargate

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -22,29 +22,5 @@ export class CdkStack extends Stack {
       })
     });
 
-    const vpc = new Vpc(this, "MyVpc", {
-      maxAzs: 3 // Default is all AZs in region
-    });
-
-    const cluster = new Cluster(this, "MyCluster", {
-      vpc: vpc
-    });
-
-    // create an image from ../Dockerfile
-    const image = new DockerImageAsset(this, 'DockerImage', {
-      directory: '../',
-      exclude: ['cdk.out', 'cdk'],
-      ignoreMode: IgnoreMode.DOCKER,
-    });
-
-    // Create a load-balanced Fargate service and make it public
-    new ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster, // Required
-      cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
-      taskImageOptions: { image: ContainerImage.fromDockerImageAsset(image) },
-      memoryLimitMiB: 2048, // Default is 512
-      publicLoadBalancer: true // Default is false
-    });
   }
 }
